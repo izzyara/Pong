4c9ec2f06f1f5d46fb6f2b1f5b1823614770fc7e
izzyara <izzybzb@gmail.com>
2024-05-21 15:01:34 -0400

    a

--- PongPaddle.java
+++ PongPaddle.java
@@ -1,3 +1,31 @@
 public class PongPaddle {
-    
+    private int y,x,width,height;
+
+    public PongPaddle(int top, int left, int w, int h){
+        y = top;
+        x = left;
+        width = w;
+        height = h; 
+    }
+    public int getBottomY(){
+        return y + height -1 ;
+    }
+    public int getLeftX(){
+        
+        return x; 
+    }
+    public int getRightX(){
+
+        return x + width -1 ;
+    }
+    public int getTopY(){
+        
+        return y;
+    }
+    public void moveDown(int d){
+        y += d;
+    }
+    public void moveUp(int d){
+        y -= d;
+    }
 }
